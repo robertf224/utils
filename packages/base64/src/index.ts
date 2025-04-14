5a93b56d52f286f65511733298a9a5716d767f30
Robert Fidler <robertf224@gmail.com>
2025-04-14 11:47:25 -0400

    export error

--- packages/base64/src/index.ts
+++ packages/base64/src/index.ts
@@ -1,7 +1,9 @@
 import type { Result } from "@bobbyfidz/result";
-import type { Base64DecodingError } from "./Base64DecodingError.js";
+import { Base64DecodingError } from "./Base64DecodingError.js";
 
 export declare const Base64: {
     encode: (input: string) => string;
     decode: (input: string) => Result<string, Base64DecodingError>;
 };
+
+export { Base64DecodingError };
